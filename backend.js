0f36b4fa11484e9f9465e10c881d1c7d2a2dcef6
html1101 <scross@keystoneschool.org>
2021-05-23 16:09:33 -0500

    Worked on create account, backend, Express integration.

--- backend.js
+++ backend.js
@@ -1,29 +1,25 @@
-// Importing builtin libraries
-const https = require("https")
+// Managing the server via expressJS
 
-// Constants
-const host = "localhost",
-    port = 8080,
-    mime_types = {
-        "html": "text/html",
-        "css": "text/css",
-        "js": "text/javascript",
-        "csv": "text/csv",
-        "jpg": "image/jpeg",
-        "ico": "image/vnd.microsoft.icon",
-        "txt": "text/plain"
-    }
+const express = require('express');
+const path = require('path');
 
-// Create request listener
-const requestListener = (req, res) => {
-    // Check if this is a URL or a file being requested
-    console.log(req.url, req)
-    res.writeHead(200, { "Content-Type": mime_types[req.url] || "text/plain" })
+const app = express(); // Initiate express app
+const port = process.env.PORT || 8080; // Create port to the environmental variable PORT, or, if that's not set, port 8080
 
-}
+// When the user accesses the home page, give them the index.html file inside the folder frontend.
+app.get('/', function(req, res) {
+    res.contentType(path.basename("home.html")) // Set the MIME type(tells the browser whether it's an HTML, CSS, JS, ICO file, etc)
+    res.sendFile(path.join(__dirname, 'frontend/home.html'));
+});
 
-const server = https.createServer(requestListener)
-
-server.listen(port, host, () => {
-    console.log(`Server is running on https://${host}:${port}`)
+// Now, when another file(filename) is being requested, give them the file inside the folder frontend(frontend/filename).
+app.get('/:filename', function(req, res) {
+    // Make sure the filename doesn't contain ".."(the same as the command cd .., allows users to go backwards in the directory and access files they're not allowed to)
+    filename = req.params.filename.split("..").join()
+    res.contentType(path.basename(filename)) // Set the MIME type(tells the browser whether it's an HTML, CSS, JS, ICO file, etc)
+    res.sendFile(path.join(__dirname, `frontend/${filename}`))
 })
+
+// Connect the Express app to localhost:port.
+app.listen(port);
+console.log('Server started at http://localhost:' + port);
